7c480980fdbe0314a1a6700347fcb2076bc730fb
hamoebgebey <hamosource@gmail.com>
2025-01-20 14:08:11 +0200

    Update play.py

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -42,7 +42,24 @@ from AlexaMusic.utils.database import is_served_user
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
-@app.on_message(filters.command(PLAY_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(
+        [
+            "play",
+            "تشغيل",
+            "شغل",
+            "vplay",
+            "فديو",
+            "cplay",
+            "cvplay",
+            "playforce",
+            "vplayforce",
+            "cplayforce",
+            "cvplayforce",
+        ],""
+    )
+    & ~BANNED_USERS
+)
 @PlayWrapper
 async def play_commnd(
     client,
